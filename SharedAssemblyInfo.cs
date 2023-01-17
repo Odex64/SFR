89dc4214292a5d7561c141617646ae9084a19a85
Odex64 <odex64@outlook.com>
2023-01-17 18:02:23 +0100

    Assembly file version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Superfighters Redux")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
