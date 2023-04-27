9108a0ce89176d7b2510d535b30779442b5905b3
Odex64 <odex64@outlook.com>
2023-04-27 18:45:46 +0200

    x86

--- Editor/Pipeline/ItemExporter.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Editor.Pipeline;
-
-public class ItemExporter
-{
-    public static void Load(string path) { }
-}
--- build.bat
+++
@@ -1,6 +0,0 @@
-SET SFD="C:\Program Files (x86)\Steam\steamapps\common\Superfighters Deluxe"
-cd ..\..\..\
-
-copy SFR\bin\%1\SFR.exe.config %SFD%
-copy SFR\bin\%1\SFR.exe %SFD%
-copy SFR\bin\%1\0Harmony.dll %SFD%\SFR
