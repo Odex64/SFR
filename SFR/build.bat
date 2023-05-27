8c9a86f47724804feb6db73265ba822983408da4
Odex <odex64@outlook.com>
2023-05-27 17:54:14 +0200

    Upgraded Scripting API. Now user scripts are compiled in latest C# version.

--- SFR/build.bat
+++ SFR/build.bat
@@ -4,3 +4,10 @@ cd ..
 copy %1\SFR.exe.config %SFD%
 copy %1\SFR.exe %SFD%
 copy %1\0Harmony.dll %SFD%\SFR
+copy %1\Microsoft.CodeAnalysis.CSharp.dll %SFD%\SFR
+copy %1\Microsoft.CodeAnalysis.dll %SFD%\SFR
+copy %1\System.Collections.Immutable.dll %SFD%\SFR
+copy %1\System.Memory.dll %SFD%\SFR
+copy %1\System.Numerics.Vectors.dll %SFD%\SFR
+copy %1\System.Reflection.Metadata.dll %SFD%\SFR
+copy %1\System.Runtime.CompilerServices.Unsafe.dll %SFD%\SFR
