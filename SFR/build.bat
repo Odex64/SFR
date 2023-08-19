731b632c2f2ca2d82cf2687f8419343da20e0335
Odex64 <odex64@outlook.com>
2023-08-19 09:51:49 +0200

    Harmony bump, removed scripts compilation modification

--- SFR/build.bat
+++ SFR/build.bat
@@ -4,10 +4,9 @@ cd ..
 copy %1\SFR.exe.config %SFD%
 copy %1\SFR.exe %SFD%
 copy %1\0Harmony.dll %SFD%\SFR
-copy %1\Microsoft.CodeAnalysis.CSharp.dll %SFD%\SFR
-copy %1\Microsoft.CodeAnalysis.dll %SFD%\SFR
-copy %1\System.Collections.Immutable.dll %SFD%\SFR
-copy %1\System.Memory.dll %SFD%\SFR
-copy %1\System.Numerics.Vectors.dll %SFD%\SFR
-copy %1\System.Reflection.Metadata.dll %SFD%\SFR
-copy %1\System.Runtime.CompilerServices.Unsafe.dll %SFD%\SFR
+copy %1\Mono.Cecil.dll %SFD%\SFR
+copy %1\MonoMod.Backports.dll %SFD%\SFR
+copy %1\MonoMod.Core.dll %SFD%\SFR
+copy %1\MonoMod.Iced.dll %SFD%\SFR
+copy %1\MonoMod.ILHelpers.dll %SFD%\SFR
+copy %1\MonoMod.Utils.dll %SFD%\SFR
